d8dd283844a92c4dd946966c6ac2db78db9b524b
kirillmihalych <tiltlessman@gmail.com>
2024-09-01 23:50:07 +0300

    [1] setup the project

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup>
+import MainLogo from './assets/logos/MainLogo.vue'
+</script>
+
+<template>
+  <div>
+    <header>
+      <MainLogo />
+      <h1>The Epic Stack</h1>
+      <p>Check the <a href="">Getting Started</a> guide file for how to get your project off the ground!</p>
+    </header>
+    <ul>
+      <li>
+        <img src="../src/assets/imgs/Faker.png" alt="" />
+      </li>
+    </ul>
+  </div>
+</template>
+
+<style scoped></style>
