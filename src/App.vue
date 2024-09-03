9da21f2698d9ad1a692cfa1d046648cd81b93319
kirillmihalych <tiltlessman@gmail.com>
2024-09-03 15:39:49 +0300

    [4] pixel perfect layout complited

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,45 @@
 <script setup>
 import MainLogo from './assets/logos/MainLogo.vue'
 import logos from './assets/logos/logos'
+
+const cols = {
+  1: 'xl:col-start-1',
+  2: 'xl:col-start-2',
+  3: 'xl:col-start-3',
+  4: 'xl:col-start-4',
+  5: 'xl:col-start-5',
+}
+
+const rows = {
+  1: 'xl:row-start-1',
+  2: 'xl:row-start-2',
+  3: 'xl:row-start-3',
+  4: 'xl:row-start-4',
+  5: 'xl:row-start-5',
+  6: 'xl:row-start-6',
+}
+
+const findClass = (classes) => classes
 </script>
 
 <template>
   <div class="container grid min-h-dvh place-items-center px-4 py-16">
-    <div class="layout grid place-items-center gap-12">
-      <header class="flex flex-col gap-4 items-center text-center">
-        <!-- <header class="grid place-items-center gap-4 text-center"> -->
+    <div class="layout grid place-items-center gap-12 sm:gap-16 xl:grid-cols-[auto_1fr] xl:gap-24">
+      <header class="grid place-items-center text-center xl:order-2 xl:place-items-start xl:text-left">
         <MainLogo class="size-20" />
-        <h1 class="text-4xl font-medium">The <span class="text-highlight">Epic</span> Stack</h1>
-        <p class="text-slate-600 max-w-md">
+        <h1 class="text-4xl font-medium mt-4 sm:text-4.5xl sm:mt-6 md:text-5xl md:mt-8 lg:text-5.5xl">
+          The <span class="text-highlight">Epic</span> Stack
+        </h1>
+        <p class="text-slate-600 mt-4 max-w-md sm:text-lg md:text-xl md:mt-6">
           Check the <a href="#" class="text-black underline">Getting Started</a> guide file for how to get your project
           off the ground!
         </p>
       </header>
-      <ul class="grid grid-cols-3 justify-items-center gap-2 max-w-3xl">
-        <li v-for="logo in logos" :key="logo.href">
-          <a :href="logo.href" class="grid place-content-center size-20 p-4 rounded-2xl bg-highlight bg-opacity-[0.07]">
+      <ul
+        class="flex justify-center flex-wrap gap-2 max-w-3xl sm:grid-cols-4 sm:gap-4 md:grid-cols-6 lg:grid-cols-7 xl:grid xl:grid-cols-5 xl:grid-rows-6 xl:grid-flow-col"
+      >
+        <li v-for="logo in logos" :key="logo.href" :class="findClass([cols[logo.column], rows[logo.row]])">
+          <a :href="logo.href" class="grid size-20 place-items-center rounded-2xl p-4 bg-highlight/[7%] sm:size-24">
             <img :src="logo.src" :alt="logo.alt" />
           </a>
         </li>
--- src/assets/logos/logos.js
+++ src/assets/logos/logos.js
@@ -15,7 +15,7 @@ import logoSentry from './logo-sentry.svg'
 import logoShadcn from './logo-shadcn.svg'
 import logoSqlite from './logo-sqlite.svg'
 import logoTailwind from './logo-tailwind.svg'
-import logoTestingLibrary from './logo-testing-library.svg'
+import logoTestingLibrary from './logo-testing-library.png'
 import logoTs from './logo-ts.svg'
 import logoVitests from './logo-vitests.svg'
 import logoZod from './logo-zod.svg'
@@ -25,106 +25,148 @@ const logos = [
     src: logoRemix,
     alt: 'remix',
     href: 'https://remix.run',
+    column: 1,
+    row: 1,
   },
   {
     src: logoFly,
     alt: 'Fly.io',
     href: 'https://resend.com',
+    column: 1,
+    row: 2,
   },
   {
     src: logoSqlite,
     alt: 'sqlite',
     href: 'https://sqlite.org',
+    column: 1,
+    row: 3,
   },
   {
     src: logoPrisma,
     alt: 'Prisma',
     href: 'https://prisma.io',
+    column: 2,
+    row: 2,
   },
   {
     src: logoZod,
     alt: 'zod',
     href: 'https://zod.dev/',
+    column: 2,
+    row: 3,
   },
   {
     src: logoGithub,
     alt: 'github',
     href: 'https://github.com',
+    column: 2,
+    row: 4,
   },
   {
     src: logoResend,
     alt: 'resend',
     href: 'https://resend.com',
+    column: 2,
+    row: 5,
   },
   {
     src: logoReactEmail,
     alt: 'react-email',
     href: 'https://react.email',
+    column: 2,
+    row: 6,
   },
   {
     src: logoTailwind,
     alt: 'tailwind',
     href: 'https://tailwindcss.com',
+    column: 3,
+    row: 3,
   },
   {
     src: logoRadix,
     alt: 'Radix',
     href: 'https://www.radix-ui.com/',
+    column: 3,
+    row: 4,
   },
   {
     src: logoShadcn,
     alt: 'shadcn',
     href: 'https://ui.shadcn.com/',
+    column: 3,
+    row: 5,
   },
   {
     src: logoPlaywright,
     alt: 'playwright',
     href: 'https://playwright.dev/',
+    column: 4,
+    row: 1,
   },
   {
     src: logoMsw,
     alt: 'msw',
     href: 'https://mswjs.io',
+    column: 4,
+    row: 2,
   },
   {
     src: logoFaker,
     alt: 'Faker',
     href: 'https://fakerjs.dev/',
+    column: 4,
+    row: 3,
   },
   {
     src: logoVitests,
     alt: 'vitests',
     href: 'https://vitest.dev',
+    column: 4,
+    row: 4,
   },
   {
     src: logoTestingLibrary,
     alt: 'testing-library',
     href: 'https://testing-library.com',
+    column: 4,
+    row: 5,
   },
   {
     src: logoDocker,
     alt: 'Docker',
     href: 'https://www.docker.com/',
+    column: 4,
+    row: 6,
   },
   {
     src: logoTs,
     alt: 'type-script',
     href: 'https://typescriptlang.org',
+    column: 5,
+    row: 2,
   },
   {
     src: logoPrettier,
     alt: 'prettier',
     href: 'https://prettier.io',
+    column: 5,
+    row: 3,
   },
   {
     src: logoEslint,
     alt: 'ESlint',
     href: 'https://eslint.org/',
+    column: 5,
+    row: 4,
   },
   {
     src: logoSentry,
     alt: 'sentry',
     href: 'https://sentry.io',
+    column: 5,
+    row: 5,
   },
 ]
 
